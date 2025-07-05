252f6bc03f9f755bea316488a125c434edcade0d
Preet Talati <talatipreet@gmail.com>
2025-07-05 13:12:21 -0700

    remove

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,36 +1,24 @@
-import React, { useState } from 'react';
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import { lightTheme, darkTheme } from './theme';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import About from './components/About';
-import Experience from './components/Experience';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
-import Education from './components/Education';
-import Footer from './components/Footer';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
-
-  const handleThemeChange = () => {
-    setDarkMode(!darkMode);
-  };
-
   return (
-    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-      <CssBaseline />
-      <Header handleThemeChange={handleThemeChange} darkMode={darkMode} />
-      <main>
-        <Hero />
-        {/* <About />
-        <Experience />
-        <Skills />
-        <Projects />
-        <Education /> */}
-      </main>
-      <Footer />
-    </ThemeProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
