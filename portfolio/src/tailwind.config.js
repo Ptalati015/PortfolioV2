fdf2095dbb35cbf853a8e66abd5eca21a8c3542a
Preet Talati <talatipreet@gmail.com>
2025-07-04 19:17:51 -0700

    init commit

---
+++ portfolio/src/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+// This is a Tailwind CSS configuration file for a React project.
+// It enables dark mode support, specifies the content files to scan for class names,
